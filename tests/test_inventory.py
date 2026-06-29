8909ff636675c37b7e6d6b895fc3a407c49ea4c5
Unserved-sleep <128551791+Unserved-sleep@users.noreply.github.com>
2026-06-29 12:53:18 +0530

    EcomAutomation[Aditya] inventoryPage_Fixture

---
+++ tests/test_inventory.py
@@ -0,0 +1,17 @@
+from pages.inventory_page import InventoryPage
+from pages.login_page import LoginPage
+
+def test_inventory_page(page):
+    login = LoginPage(page)
+    login.open_login_page()
+    login.login("standard_user", "secret_sauce")
+    inv = InventoryPage(page)
+
+    assert inv.item_count() == 6
+
+    inv.add_to_cart("Sauce Labs Bike Light")
+    inv.add_to_cart("Sauce Labs Bolt T-Shirt")
+    inv.remove_from_cart("Sauce Labs Bike Light")
+    inv.add_to_cart("Sauce Labs Fleece Jacket")
+
+    assert int(inv.cart_items()) == 2
